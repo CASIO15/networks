355a2931bc3732b335b34e385d408d55762d5bfd
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-01 11:35:51 +0200

    Create Three_Way_Handshake.py

---
+++ Three_Way_Handshake.py
@@ -0,0 +1,12 @@
+from scapy.all import *
+from scapy.layers.dns import *
+
+syn_segment = TCP(dport=80, seq=123, flags='S')
+syn_packet = IP(dst='www.google.com')/syn_segment
+syn_akc_packet = sr1(syn_packet)
+
+ack_segment = IP(dst='www.google.com')/TCP(dport=80, ack=124,
+                                           seq=int(syn_akc_packet[TCP].ack) + 1, flags='A')
+
+ack_packet = IP(dst='www.google.com')/ack_segment
+send(ack_packet)
