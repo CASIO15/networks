b9abc3369d761f49699d597e5f6076c6a40197c5
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-01 20:54:47 +0200

    Update Three_Way_Handshake.py

--- Three_Way_Handshake.py
+++ Three_Way_Handshake.py
@@ -5,8 +5,10 @@ syn_segment = TCP(dport=80, seq=123, flags='S')
 syn_packet = IP(dst='www.google.com')/syn_segment
 syn_akc_packet = sr1(syn_packet)
 
-ack_segment = IP(dst='www.google.com')/TCP(dport=80, ack=124,
-                                           seq=int(syn_akc_packet[TCP].ack) + 1, flags='A')
+ack_segment = IP(dst='www.google.com')/TCP(dport=80,
+                                           ack=int(syn_akc_packet[TCP].ack) + 1,
+                                           seq=int(syn_akc_packet[TCP].seq) + 1,
+                                           flags='A')
 
 ack_packet = IP(dst='www.google.com')/ack_segment
 send(ack_packet)
