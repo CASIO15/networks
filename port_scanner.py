7f4e69da66938655c2d8121aa26630a58f485e72
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-02 02:02:50 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,9 +1,13 @@
 from scapy.layers.dns import *
+from time import perf_counter
 
 enter = input('Enter the IP address you want to scan: ')
 start = int(input('START: '))
 stop = int(input('STOP: '))
 ports = []
+
+print(f'scanning {enter} from port {start} to {stop}\n')
+start_time = perf_counter()
 
 # setting start and stop ports range for the scanning
 for i in range(start, stop + 1):
@@ -15,10 +19,10 @@ for i in range(start, stop + 1):
     syn_packet = IP(dst=enter)/syn_segment
 
     # Sending one packet, and receiving one answer, setting timeout
-    syn_ack_packet = sr1(syn_packet, timeout=0.1)
+    syn_ack_packet = sr1(syn_packet, timeout=0.1, verbose=0)
 
     # Checking if the TCP flag is SYN - ACK
-    # if exception is raised, pass
+    # if exception is raising, pass
     try:
         if syn_ack_packet[TCP].flags == 'SA':
             ports.append(syn_ack_packet[TCP].sport)
@@ -28,7 +32,10 @@ for i in range(start, stop + 1):
 
 # Printing open ports, else if there are no open ports
 if ports:
-    for open in ports:
-        print(f'Port << {open} >> is open...')
+    for o in ports:
+        print(f'Port << {o} >> is open...')
+
 else:
     print('No open ports...')
+
+print(f'Scan is complete | time: {abs(start_time - perf_counter())}')
