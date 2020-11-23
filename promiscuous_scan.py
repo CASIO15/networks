ee2d50ebabe81e943b835cad5dc2a7a6ecacd8ce
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-24 01:49:18 +0200

    Create promiscuous_scan.py

---
+++ promiscuous_scan.py
@@ -0,0 +1,32 @@
+from scapy.layers.l2 import *
+from scapy.all import *
+
+def promi_scanner():
+
+    """
+    Scan the network for promiscuous mode by sending fake ARP broadcast,
+    in order for us to check if the os system checks all the bits or whether it will respond.
+    """
+
+    enter_ip = input('Enter IP to scan: ')
+    enter_ip = '.'.join(enter_ip.split('.')[:3])
+    enter_router = input('Enter router address: ')
+
+    for i in range(256):
+        # Fake broadcast address
+        arp_packet = Ether(dst='FF:FF:FF:FF:FF:FE') / ARP(pdst=f'{enter_ip}.{i}')
+        router = enter_router
+
+        sendp(arp_packet)
+
+        sniff_it = sniff(count=1, lfilter=lambda frame: Ether in frame and ARP in frame and frame[ARP].op == 2, timeout=0.5)
+
+        if sniff_it:
+            if sniff_it[0][Ether][ARP].psrc != arp_packet[ARP].psrc and sniff_it[0][Ether][ARP].psrc != router:
+                return f'{sniff_it[0][Ether][ARP].psrc} is in promiscuous mode'
+
+            else:
+              pass
+
+print(promi_scanner())
+
