703b497552058ac89fd4f65a6f33059b1ef15202
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2020-11-30 20:59:53 +0200

    Create HACK_THE_BOX_emdee.py

---
+++ HACK_THE_BOX_emdee.py
@@ -0,0 +1,23 @@
+import requests
+import hashlib
+
+url = 'http://161.35.163.196:31289/'
+session_1 = requests.session()
+
+GET = session_1.get(url)
+print('[+] SEND GET...')
+
+sha_key_start = GET.text.find("<h3 align='center'>")
+sha_key_end = GET.text.find("</h3")
+sha_key = GET.text[sha_key_start+19:sha_key_end]
+
+print(f'[+] FIND KEY {sha_key}')
+
+sha_encrypt_res = hashlib.md5(sha_key.encode()).hexdigest()
+print(f'[+] KEY IS ENCRYPTED {sha_encrypt_res}\n')
+
+data = {'hash': sha_encrypt_res}
+
+POST = session_1.post(url, data=data)
+
+print(POST.text)
