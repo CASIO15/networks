e5319b44016d2d298035b247d0034b935daa8c04
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2021-01-29 20:36:10 +0200

    Update simple_cesar_bruteforce.py

--- simple_cesar_bruteforce.py
+++ simple_cesar_bruteforce.py
@@ -1,41 +1,81 @@
-key = 'abcdefghijklmnopqrstuvwxyz'
+import requests
+import re
 
-def decrypt(string, off_set, *, key):
-	ascii_lower = 'abcdefghijklmnopqrstuvwxyz'
+class DecryptCesar:
 
-	normal_dict = dict(enumerate(ascii_lower))
-	off_set_dict = {}
-	res = ''
+	def __init__(self, url, key, enctypted_str):
+		if 'https' not in url and 'http' not in url:
+			print(f'[-] Invalid url...')
+			raise ValueError(f'The specified url |{url}| is not valid')
 
-	for e, v in enumerate(key):
-		off_set_dict[v] = (e + off_set) % 26
+		self.url = url
+		self.key = key
+		self.encrypted_str = enctypted_str
 
-	for i in string:
-		if i != ' ':
-			try:
-				res += normal_dict.get(off_set_dict[i])
-			except KeyError:
+		self.highest_chance = set()
+		self.word_dict = {}
+
+	def get_words(self):
+		GET = requests.get(self.url)
+		print(f'[+] Making a GET from: {self.url}')
+		for e, v in enumerate(re.findall(r'>[a-zA-Z]+</td>', GET.text)):
+			stop = v.find('<')
+			self.word_dict[v[1:stop]] = e
+
+	@staticmethod
+	def decrypt(encrypted, off_set, *, key):
+		encrypted = encrypted.lower()
+		res = ''
+
+		off_set_dict = {}
+		regular_dict = dict(enumerate('abcdefghijklmnopqrstuvwxyz'))
+
+		for e, v in enumerate(key):
+			off_set_dict[v] = (e + off_set) % 26
+
+		for i in encrypted:
+			if i != ' ':
+				try:
+					res += regular_dict.get(off_set_dict[i])
+				except KeyError:
+					res += i
+			else:
 				res += i
-		else:
-			res += i
 
-	return res
+		return res
 
-def brute_force(string):
+	def brute_force(self):
 
-	word_list = ['a', 'i', 'me', 'and', 'the', 'it', 'they', 'are', 'if']
-	best_options = set()
+		count = 0
+		lst_cnt = []
 
-	for i in range(1, 26):
-		for x in decrypt(string, i, key=key).split(' '):
-			if x in word_list:
-				best_options.add(decrypt(string, i, key=key))
-
-	if not best_options:
 		for i in range(1, 26):
-			print(decrypt(string, i, key=key))
+			for x in DecryptCesar.decrypt(self.encrypted_str, i, key=self.key).split(' '):
+				if x in self.word_dict:
+					count += 1
+			if count > 1:
+				self.highest_chance.add(self.decrypt(self.encrypted_str, i, key=self.key))
 
-	return best_options
+			count = 0
 
-print(brute_force("FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV; WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH.".lower()))
-# Result: {'cowards die many times before their deaths; the valiant never taste of deathbut once.'}
+		if not self.highest_chance:
+			for i in range(1, 26):
+				print(self.decrypt(self.encrypted_str, i, key=self.key))
+
+		return 'Most likely --> ' + str(self.highest_chance).strip('{').strip('}')
+
+
+def main():
+
+	key = 'abcdefghijklmnopqrstuvwxyz'
+	url = 'https://gist.github.com/deekayen/4148741'
+	encrypted = "FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV; WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH."
+
+	test1 = DecryptCesar(url=url, key=key, enctypted_str=encrypted)
+	test1.get_words()
+	print(test1.brute_force())
+
+	# Result: {'cowards die many times before their deaths; the valiant never taste of death but once.'}
+
+if __name__ == '__main__':
+	main()
