41cfcdf099b5605da86a13ad1f7ec4259695213d
CASIO15 <71800501+CASIO15@users.noreply.github.com>
2021-01-29 13:35:40 +0200

    Update simple_cesar_bruteforce.py

--- simple_cesar_bruteforce.py
+++ simple_cesar_bruteforce.py
@@ -1,12 +1,12 @@
-def decrypt(string, off_set):
+def decrypt(string, off_set, *, key):
 	ascii_lower = 'abcdefghijklmnopqrstuvwxyz'
 
 	normal_dict = dict(enumerate(ascii_lower))
 	off_set_dict = {}
 	res = ''
 
-	for e, v in enumerate(ascii_lower):
-		off_set_dict[v] = (e - off_set) % 26
+	for e, v in enumerate(key):
+		off_set_dict[v] = (e + off_set) % 26
 
 	for i in string:
 		if i != ' ':
@@ -22,10 +22,10 @@ def brute_force(string):
 	best_options = set()
 
 	for i in range(1, 26):
-		for x in decrypt(string, i).split(' '):
+		for x in decrypt(string, i, key='icghkxpwstujzdleqmbvofanry').split(' '):
 			if x in word_list:
-				best_options.add(decrypt(string, i))
+				best_options.add(decrypt(string, i, key='icghkxpwstujzdleqmbvofanry'))
 
 	return best_options
 
-print(brute_force('jxu gkuijyed ev mxujxuh q secfkjuh sqd jxyda yi de cehu ydjuhuijydw jxqd jxu gkuijyed ev mxujxuh q ikrcqhydu sqd imyc'))
+print(brute_force("fbyyg ugxym ap l paihyb hxgoxli"))
