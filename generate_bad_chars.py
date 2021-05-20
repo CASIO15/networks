fa4236074df442d44ae658b3bcedbf401e3f5ad3
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-05-20 10:19:39 -0400

    Create generate_bad_chars.py

---
+++ generate_bad_chars.py
@@ -0,0 +1,27 @@
+import sys
+
+def _help():
+	return 'Usage - python3 <filename.py> <hex to filter> [x01 x02 x03...]'
+
+def bad_chars():
+	x = ''
+
+	if len(sys.argv):
+		args = [int('0'+i, 16) for i in sys.argv[1:]]
+
+	for i in range(1, 256):
+		if len(sys.argv) and any([True for x in args if i == x]):
+			continue
+		else:
+			x += '\\x{:02x}'.format(i)
+
+	return x
+
+def main():
+	if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+		print(_help())
+	else:
+		print(bad_chars())
+
+if __name__ == '__main__':
+	main()
